db93981877f559810e3e4d080286133a5ef8a906
grodmir <dionislobacevich@gmail.com>
2026-09-20 00:34:55 +0300

    The project has been initialized.

---
+++ src/main/java/by/grodmir/IT_project_hub/ItProjectHubApplication.java
@@ -0,0 +1,13 @@
+package by.grodmir.IT_project_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItProjectHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItProjectHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/grodmir/IT_project_hub/ItProjectHubApplicationTests.java
@@ -0,0 +1,13 @@
+package by.grodmir.IT_project_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItProjectHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
