a1ce1ad79c6010247eeb97922c5d7c4147dd5d4e
grodmir <dionislobacevich@gmail.com>
2026-09-22 14:57:07 +0300

    deleted redunant classes

--- src/main/java/by/grodmir/IT_project_hub/repository/ProgrammerRepository.java
+++
@@ -1,8 +0,0 @@
-package by.grodmir.IT_project_hub.repository;
-
-import by.grodmir.IT_project_hub.entity.Programmer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProgrammerRepository extends JpaRepository<Programmer, Long> {
-
-}
--- src/main/java/by/grodmir/IT_project_hub/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package by.grodmir.IT_project_hub.repository;
-
-import by.grodmir.IT_project_hub.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
